ab933f7b00fb1bd6f09e18aecf29a924e35d9117
Victor Cadillo <45715531+vcadillog@users.noreply.github.com>
2023-03-18 17:29:42 -0500

    Update learning.py

--- utils/learning.py
+++ utils/learning.py
@@ -11,7 +11,7 @@ from networks import EnsembleNet, ImgNet,TabularNet
 from torchvision.transforms import ToTensor, Compose, Resize,Normalize
 
 data_transforms =  Compose([                 
-                          Resize([150,200]),                          
+                          Resize([100,100]),                          
                           ToTensor(),
                           Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
                           ])   
