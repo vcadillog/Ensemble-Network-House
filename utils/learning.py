7208d05d1e5e4d7aa628b8a2539a006ae63a0489
Victor Cadillo <45715531+vcadillog@users.noreply.github.com>
2023-03-18 14:34:37 -0500

    Update learning.py

--- utils/learning.py
+++ utils/learning.py
@@ -1,76 +1,195 @@
+import random
 import torch
+import numpy as np
+import wandb
 import torch.nn as nn
-import torchvision.models as models
 
-class TabularNet(nn.Module):
-    def __init__(self, num_features, net_mode = 0):
-        super(TabularNet, self).__init__()
-        self.mode = net_mode
-        self.batchn1 = nn.BatchNorm1d(512)
-        self.batchn2 = nn.BatchNorm1d(128)        
-        
-        self.fc1 = nn.Linear(num_features, 512)
-        self.fc2 = nn.Linear(512, 128)        
-        self.output = nn.Linear(128, 1)
-        self.dropout = nn.Dropout(0.2)
-        self.relu = nn.ReLU()
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader
+from dataset import CustomDataset
+from networks import EnsembleNet, ImgNet,TabularNet
+from torchvision.transforms import ToTensor, Compose, Resize,Normalize
 
-    def forward(self, x):        
-        x = self.fc1(x)
-        x = self.batchn1(x)        
-        x = self.relu(x)
-        x = self.dropout(x)
+data_transforms =  Compose([                 
+                          Resize([150,200]),                          
+                          ToTensor(),
+                          Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+                          ])   
 
-        x = self.fc2(x)
-        x = self.batchn2(x)        
-        x = self.relu(x)
-        x = self.dropout(x)
+def set_seed(seed):
+  torch.manual_seed(seed)
+  random.seed(seed)
+  np.random.seed(seed)
 
-        if self.mode:
-          x = self.output(x)
-        return x
+def epoch_train(mode,model,device,optimizer,criterion,train_loader):
+    model.train()
+    train_loss = []
+    if mode == 'ensemble':      
+      for tmp_meta,tmp_img, tmp_y in train_loader:
 
-class ImgNet(nn.Module):
-    def __init__(self, net_mode = 0):
-        super(ImgNet, self).__init__()
-        self.mode = net_mode
-        efficientnet = models.efficientnet_v2_s(pretrained=True)
-        self.features = nn.Sequential(*list(efficientnet.children())[:-1]) #Output: 1280
-        self.pooling = nn.AdaptiveAvgPool2d((1, 1))
-        if self.mode:
-          self.tabular_net = TabularNet(num_features = 1280 , net_mode = 1)
+          tmp_meta,tmp_img,tmp_y = tmp_meta.to(device) , tmp_img.to(device) , tmp_y.float().view(-1,1).to(device)
+
+          optimizer.zero_grad()              
+          outputs = model(tmp_meta,tmp_img)          
+          loss = criterion(outputs, tmp_y)
+          loss.backward()
+          optimizer.step()
+          loss_np = loss.detach().cpu().numpy()
+          train_loss.append(loss_np)        
+    elif mode == 'meta':      
+      for tmp_meta, tmp_y in train_loader:
+
+          tmp_meta,tmp_y = tmp_meta.to(device) , tmp_y.float().view(-1,1).to(device)
+
+          optimizer.zero_grad()              
+          outputs = model(tmp_meta)          
+          loss = criterion(outputs, tmp_y)
+          loss.backward()
+          optimizer.step()          
+          loss_np = loss.detach().cpu().numpy()
+          train_loss.append(loss_np)          
+    else:      
+      for tmp_img, tmp_y in train_loader:
+
+          tmp_img,tmp_y = tmp_img.to(device) , tmp_y.float().view(-1,1).to(device)
+
+          optimizer.zero_grad()              
+          outputs = model(tmp_img)          
+          loss = criterion(outputs, tmp_y)
+          loss.backward()
+          optimizer.step()
+          loss_np = loss.detach().cpu().numpy()
+          train_loss.append(loss_np)        
+
+    return model,optimizer,train_loss
+
+def eval_model(mode,model,device,criterion,val_loader):
+    model.eval()
+    val_loss = []
+    if mode == 'ensemble':
+      with torch.no_grad():
+          for tmp_meta,tmp_img, tmp_y in val_loader:
+
+              tmp_meta,tmp_img,tmp_y = tmp_meta.to(device) , tmp_img.to(device) , tmp_y.float().view(-1,1).to(device)
+
+              outputs = model(tmp_meta,tmp_img)
+              
+              loss = criterion(outputs, tmp_y)
+              val_loss.append(loss.detach().cpu().numpy())
+    elif mode == 'meta':
+      with torch.no_grad():
+          for tmp_meta,tmp_y in val_loader:
+
+              tmp_meta,tmp_y = tmp_meta.to(device), tmp_y.float().view(-1,1).to(device)
+
+              outputs = model(tmp_meta)              
+              loss = criterion(outputs, tmp_y)              
+              val_loss.append(loss.detach().cpu().numpy())
+    else:
+      with torch.no_grad():
+          for tmp_img, tmp_y in val_loader:
+
+              tmp_img,tmp_y = tmp_img.to(device) , tmp_y.float().view(-1,1).to(device)
+
+              outputs = model(tmp_img)              
+              loss = criterion(outputs, tmp_y)
+              val_loss.append(loss.detach().cpu().numpy())
+                          
+    return val_loss
+
+def reload_model(mode,X,y):
+  if mode == 'ensemble':
+    dataset = CustomDataset(mode=mode,meta = X,dir_path = './Houses-dataset/Houses Dataset', y = y, transform= data_transforms)
+    model = EnsembleNet(tabular_num_features=X.shape[1])
+  elif mode == 'meta':
+    dataset = CustomDataset(mode=mode,meta = X, y = y)
+    model = TabularNet(num_features=X.shape[1]  , net_mode = 1)
+  else:
+    dataset = CustomDataset(mode=mode,dir_path = './Houses-dataset/Houses Dataset', y = y, transform= data_transforms)
+    model = ImgNet( net_mode = 1)  
+  return model, dataset
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 
-    def forward(self, x):
-        
-        x = self.features(x)
-        x = self.pooling(x)
-        x = x.view(x.size(0), -1)
-        if self.mode:
-          x = self.tabular_net(x)        
-        return x
+def run(mode, X,y, num_epochs = 20, batch_size = 32, num_folds = 5):
+  
+  assert mode in ['meta','image','ensemble']
+  
+  
+  # Define the number of epochs and number of folds for cross validation
+  seed = 42
+  num_folds = num_folds
+  batch_size = batch_size
+  set_seed(seed)
+  criterion = nn.MSELoss()
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  # Initialize Weight and Biases
+  wandb.init(project='House')  
 
-class EnsembleNet(nn.Module):
-    def __init__(self, tabular_num_features):
-        super(EnsembleNet, self).__init__()
-        
-        self.tabular_net = TabularNet(tabular_num_features) #Returns 128
+  # Perform cross validation
+  kf = KFold(n_splits=num_folds)
+  _ , dataset = reload_model(mode,X,y)
 
-        self.image_net = ImgNet() #Returns 1280
-        self.relu = nn.ReLU()
-               
-        self.fc1 = nn.Linear(1408, 512)
-        
-        self.output = nn.Linear(512, 1)
-        self.dropout = nn.Dropout(0.2)
+  best_fold_loss = []
+  best_fold_model = []
 
-    def forward(self, x_tabular, x_image):
-        x_tabular = self.tabular_net(x_tabular)
-        x_image = self.image_net(x_image)
-        x = torch.cat((x_tabular,x_image), dim=1)
-       
-        x = self.fc1(x)          
-        x = self.relu(x)
-        x = self.dropout(x)        
-        x = self.output(x)
-        return x
+  
+
+  for fold, (train_idx, val_idx) in enumerate(kf.split(dataset)):
+      model, _ = reload_model(mode,X,y)
+
+      model = model.to(device)      
+      optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4 , weight_decay=0.1)    
+
+      print(f'Fold {fold + 1}')
+
+      # Define the training and validation data loaders for the current fold
+
+      g = torch.Generator()
+      g.manual_seed(seed)
+
+      train_data = torch.utils.data.Subset(dataset, train_idx)
+      val_data = torch.utils.data.Subset(dataset, val_idx)
+      train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True , worker_init_fn=seed_worker, generator=g)
+      val_loader = DataLoader(val_data, batch_size=batch_size,shuffle=True, worker_init_fn=seed_worker, generator=g)
+
+      # Initialize variables for logging and tracking best model
+      best_loss = float('inf')
+      
+      best_model = None
+
+      # Train the model for the current fold
+
+      for epoch in range(num_epochs):
+
+
+          model,optimizer,train_loss  = epoch_train(mode,model,device,optimizer,criterion,train_loader)
+          val_loss = eval_model(mode,model,device,criterion,val_loader)             
+
+          train_loss = np.mean(train_loss)
+          val_loss = np.mean(val_loss)
+          wandb.log({'Train loss': train_loss,
+                     'Validation Loss': val_loss,
+                     'Epoch': epoch+1,
+                     'Fold': fold+1
+                     }) 
+
+
+          print(f'Epoch {epoch + 1}: train_loss={train_loss:.4f}, val_loss={val_loss:.4f}')
+          # Save best model
+          if val_loss < best_loss:
+              best_loss = val_loss              
+              best_model = model.state_dict()
+      
+      best_fold_loss.append(best_loss)
+      best_fold_model.append(best_model)
+
+
+      # Save best model after each fold
+  best_idx = np.argmin(best_fold_loss)  
+  best_model = best_fold_model[best_idx]
+  torch.save(best_model, f'best_{mode}_model.pt')
